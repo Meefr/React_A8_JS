5d5ac2fb5711759709add5762f10ae35866bc56c
Meefr <mohamedelramah737@gmail.com>
2024-07-11 21:03:55 +0300

    adding selection of each content

--- js/main.js
+++ js/main.js
@@ -86,5 +86,90 @@ function fillAllData() {
     itemContainer.appendChild(itemContent);
     item.appendChild(itemContainer);
     recipeItem.appendChild(item);
+    item.onclick = () => productItemGenerator(pizza_data.recipes[i].recipe_id);
   }
 }
+
+function productItemGenerator(id) {
+  console.log(id);
+  var productContainer = document.querySelector("#product-container");
+  var productItem = document.querySelector("#product-item");
+  // productItem.innerHTML = "";
+  var itemData = null;
+  fetch(base_url + `/get?rId=${id}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      itemData = data;
+      productContainer.classList.replace("d-none", "d-block");
+      productItem.innerHTML = "";
+      var itemContainer = document.createElement("div");
+      itemContainer.className = "card";
+      itemContainer.style = "width: 18rem";
+      var itemImg = document.createElement("img");
+      itemImg.src = itemData.recipe.image_url;
+      itemImg.className = "card-img-top";
+      itemImg.alt = itemData.recipe.publisher;
+      itemContainer.appendChild(itemImg);
+      var itemContent = document.createElement("div");
+      itemContent.className = "card-body";
+      var itemTitle = document.createElement("h5");
+      itemTitle.className = "card-title";
+      itemTitle.innerHTML = itemData.recipe.title;
+      itemContent.appendChild(itemTitle);
+      var itemText = document.createElement("p");
+      itemText.className = "card-text";
+      itemText.innerHTML = `Publisher: ${itemData.recipe.publisher}`;
+      itemContent.appendChild(itemText);
+      var itemRank = document.createElement("p");
+      itemRank.className = "card-text";
+      itemRank.innerHTML = `Social Rank: ${itemData.recipe.social_rank}`;
+      itemContent.appendChild(itemRank);
+      itemContainer.itemContent;
+
+      var itemListGroup = document.createElement("ul");
+      itemListGroup.className = "list-group list-group-flush scrollable-list";
+      itemListGroup.innerHTML = "";
+      for (let i = 0; i < itemData.recipe.ingredients.length; i++) {
+        var itemIngredient = document.createElement("li");
+        itemIngredient.className = "list-group-item";
+        itemIngredient.innerHTML = itemData.recipe.ingredients[i];
+        itemListGroup.appendChild(itemIngredient);
+      }
+
+      itemContainer.appendChild(itemListGroup);
+      var itemCardBody = document.createElement("div");
+      itemCardBody.className = "card-body";
+      var itemlink1 = document.createElement("a");
+      itemlink1.className = "btn btn-primary";
+      itemlink1.innerHTML = "Source";
+      itemlink1.href = itemData.recipe.source_url;
+      itemCardBody.appendChild(itemlink1);
+      var itemlink2 = document.createElement("a");
+      itemlink2.className = "btn btn-primary";
+      itemlink2.innerHTML = "Publisher";
+      itemlink2.href = itemData.recipe.publisher_url;
+      itemCardBody.appendChild(itemlink2);
+      itemContainer.appendChild(itemCardBody);
+      // console.log(itemContainer);
+      var itemButton = document.createElement("button");
+      itemButton.className = "btn-close";
+      itemButton.ariaLabel = "Close";
+      itemButton.id = "product-close-button";
+      productItem.appendChild(itemButton)
+      productItem.appendChild(itemContainer);
+
+      var closeButton = document.querySelector("#product-close-button");
+      closeButton.onclick = () => {
+        console.log(1);
+        var productItem = document.querySelector("#product-container");
+        console.log(productItem.className);
+        productItem.classList.replace("d-block", "d-none");
+        console.log(productItem.className);
+      };
+
+      // console.log(productItem)
+    });
+}
+
